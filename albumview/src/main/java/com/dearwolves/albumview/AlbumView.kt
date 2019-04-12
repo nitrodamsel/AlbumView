2b2fd11ee58c363823cd7696a64a0fc42ce200b2
apps <rjcristy@gmail.com>
2019-04-13 00:30:08 +0800

    updating attributes

--- albumview/src/main/java/com/dearwolves/albumview/AlbumView.kt
+++ albumview/src/main/java/com/dearwolves/albumview/AlbumView.kt
@@ -2,12 +2,16 @@ package com.dearwolves.albumview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.LayoutInflater
 import android.widget.RelativeLayout
 import android.view.View
 import android.widget.ImageView
+import android.widget.ImageView.ScaleType
 import java.util.*
 
 class AlbumView : RelativeLayout {
+
 
 
     private var f1:ImageView? = null
@@ -151,10 +155,12 @@ class AlbumView : RelativeLayout {
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         init()
+        setupAttributes(attrs)
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
         init()
+        setupAttributes(attrs)
     }
 
     private fun init() {
@@ -174,5 +180,46 @@ class AlbumView : RelativeLayout {
         this.ffff3 = findViewById(R.id.ffff3) as ImageView
         this.ffff4 = findViewById(R.id.ffff4) as ImageView
 
+    }
+
+    private fun setupAttributes(attrs: AttributeSet?) {
+        val a = context.obtainStyledAttributes(attrs, R.styleable.AlbumView, 0, 0);
+        // how to correctly get scale type defined in xml ?
+        val scaleTypeIndex = a.getInt(R.styleable.AlbumView_scaleType, 1);
+        when(scaleTypeIndex) {
+
+            1 -> {
+                f1?.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+                ff1?.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                ff2?.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+                fff1?.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                fff2?.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                fff3?.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+                ffff1?.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                ffff2?.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                ffff3?.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                ffff4?.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            }
+            else -> {
+                f1?.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+                ff1?.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                ff2?.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+                fff1?.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                fff2?.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                fff3?.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+                ffff1?.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                ffff2?.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                ffff3?.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                ffff4?.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            }
+
+        }
+        a.recycle()
     }
 }
